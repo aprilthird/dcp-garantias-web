f98b0ffd21a812afb294775b67f0213c815234e8
Luis Simon Martinez <lsimon@solera.pe>
2022-05-16 19:10:36 +0000

    Updated environment.local.ts

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -8,8 +8,8 @@ export const environment = {
   azureSas:
     "sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-03T23:39:46Z&st=2022-01-03T15:39:46Z&spr=https&sig=BU4Y9BHhsNT4EbY%2FM2eJZ7X9EYdNE4NUC9nbcSwFuc8%3D",
     officeTenant: {
-      clientIdAzure: "df3c881e-0c3f-457e-a355-8d8d0d61ea1c",
-      objectIdAzure: "ad5d0da5-a703-4e32-973e-e9358533a4a8",
+      clientIdAzure: "47da4b03-1750-4f10-a60e-a8ccd55b3746",
+      objectIdAzure: "27349a51-6e36-49b9-9ab1-ed553c1f8535",
       redirectUrl: "https://appsrvsgpwebdock.azurewebsites.net/",
       postLogoutRedirectUri: "https://appsrvsgpwebdock.azurewebsites.net/",
       tenantId: "807307b4-6a4c-4b3d-97fd-7c78330bba23",
