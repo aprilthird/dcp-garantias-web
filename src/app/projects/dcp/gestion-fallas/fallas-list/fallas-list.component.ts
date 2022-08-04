ba3727639191b726d3b259fe02e55a0aef3e7dd5
Luis Simon Martinez <lsimon@solera.pe>
2022-08-04 12:27:00 -0500

    estilos a la bitacora

--- src/app/projects/dcp/gestion-fallas/fallas-list/fallas-list.component.ts
+++ src/app/projects/dcp/gestion-fallas/fallas-list/fallas-list.component.ts
@@ -218,5 +218,15 @@ export class FallasListComponent implements OnInit {
     this.cargarFormularioBusqueda();
     this.listarFallas(true);
   }
+
+  obtenerEstilo(index:number):string{
+    let estilo:string;
+    if(index%2==0){
+      estilo = 'color-fila-gray';
+    }else{
+      estilo = 'color-fila-white'
+    }
+    return estilo;
+  }
   
 }
