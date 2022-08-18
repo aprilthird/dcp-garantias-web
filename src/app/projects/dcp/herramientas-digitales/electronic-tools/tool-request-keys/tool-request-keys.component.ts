8a49ec25c227a2cb4118dfbf4463f18acca6aa25
oloartec <oloartec@tgestiona.com.pe>
2022-08-18 12:35:35 -0500

    [update] pruebas con error 500 v2

--- src/app/projects/dcp/herramientas-digitales/electronic-tools/tool-request-keys/tool-request-keys.component.ts
+++ src/app/projects/dcp/herramientas-digitales/electronic-tools/tool-request-keys/tool-request-keys.component.ts
@@ -1,194 +1,253 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { DialogMassiveRegistrationSuccessfullyComponent } from 'app/projects/dcp/garantias/dialogs/dialog-massive-registration-successfully/dialog-massive-registration-successfully.component';
-import { DialogErrorMessageComponent } from 'app/shared/dialogs/dialog-error-message/dialog-error-message.component';
-import { SnackBarMessageComponent } from 'app/shared/dialogs/snack-bar-message/snack-bar-message.component';
-import { ConfigurationAndMaintenanceService } from 'app/shared/services/configuration-and-maintenance/configuration-and-maintenance.service';
-import { DigitalToolsService } from 'app/shared/services/digital-tools/digital-tools.service';
-import { fromEvent } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Router} from '@angular/router';
+import {
+    DialogMassiveRegistrationSuccessfullyComponent
+} from 'app/projects/dcp/garantias/dialogs/dialog-massive-registration-successfully/dialog-massive-registration-successfully.component';
+import {DialogErrorMessageComponent} from 'app/shared/dialogs/dialog-error-message/dialog-error-message.component';
+import {SnackBarMessageComponent} from 'app/shared/dialogs/snack-bar-message/snack-bar-message.component';
+import {
+    ConfigurationAndMaintenanceService
+} from 'app/shared/services/configuration-and-maintenance/configuration-and-maintenance.service';
+import {DigitalToolsService} from 'app/shared/services/digital-tools/digital-tools.service';
+import {fromEvent} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-tool-request-keys',
-  templateUrl: './tool-request-keys.component.html',
-  styleUrls: ['./tool-request-keys.component.scss']
+    selector: 'app-tool-request-keys',
+    templateUrl: './tool-request-keys.component.html',
+    styleUrls: ['./tool-request-keys.component.scss']
 })
 export class ToolRequestKeysComponent implements OnInit {
 
-  formRequest:FormGroup;
+    formRequest: FormGroup;
 
-  displayedColumns: string[] = ['tipo', 'cantidad', 'keyFuncional', 'keyBasico', 'keyActivacion'];
+    displayedColumns: string[] = ['tipo', 'cantidad', 'keyFuncional', 'keyBasico', 'keyActivacion'];
 
-  localRequest:any;
-  user:any;
-  action:string;
+    localRequest: any;
+    user: any;
+    action: string;
 
-  users = [];
-  isSearching = false;
+    users = [];
+    isSearching = false;
 
-  dataSource = [{id: 0, idHerramienta:0, tipo:'Inside', cantidad:1, keyFuncional:null, keyBasico:null, keyActivacion:null },
-                {id: 0, idHerramienta:0, tipo:'Inpower', cantidad:1, keyFuncional:null, keyBasico:null, keyActivacion:null},
-                {id: 0, idHerramienta:0, tipo:'Calibrations', cantidad:0, keyFuncional:null, keyBasico:null, keyActivacion:null},
-                {id: 0, idHerramienta:0, tipo:'Zap - Its', cantidad:0, keyFuncional:null, keyBasico:null, keyActivacion:null}];
+    dataSource = [{
+        id: 0,
+        idHerramienta: 0,
+        tipo: 'Inside',
+        cantidad: 1,
+        keyFuncional: null,
+        keyBasico: null,
+        keyActivacion: null
+    },
+        {
+            id: 0,
+            idHerramienta: 0,
+            tipo: 'Inpower',
+            cantidad: 1,
+            keyFuncional: null,
+            keyBasico: null,
+            keyActivacion: null
+        },
+        {
+            id: 0,
+            idHerramienta: 0,
+            tipo: 'Calibrations',
+            cantidad: 0,
+            keyFuncional: null,
+            keyBasico: null,
+            keyActivacion: null
+        },
+        {
+            id: 0,
+            idHerramienta: 0,
+            tipo: 'Zap - Its',
+            cantidad: 0,
+            keyFuncional: null,
+            keyBasico: null,
+            keyActivacion: null
+        }];
 
-  constructor(private readonly router:Router, private readonly matDialog:MatDialog, private readonly matSnackBar: MatSnackBar,
-    private readonly digitalToolsService:DigitalToolsService, 
-    private readonly configurationAndMaintenanceService:ConfigurationAndMaintenanceService) { }
-
-  ngOnInit(): void {
-    this.action = localStorage.getItem('action');
-    if(this.action === 'edit') {
-      let localRequestStr = localStorage.getItem('usuario');
-      if(localRequestStr !== null && localRequestStr !== "") {
-        this.localRequest = JSON.parse(localRequestStr);
-      }
-      this.loadFormRequest();
-      this.obtainLicenses();
-      this.user = {
-        bu: this.localRequest.area,
-        centroCosto: this.localRequest.ceco,
-        jefe: this.localRequest.jefe,
-        correoJefe: this.localRequest.correoJefe
-      };
-    } else {
-      this.loadEmptyFormRequest();
+    constructor(private readonly router: Router, private readonly matDialog: MatDialog, private readonly matSnackBar: MatSnackBar,
+                private readonly digitalToolsService: DigitalToolsService,
+                private readonly configurationAndMaintenanceService: ConfigurationAndMaintenanceService) {
     }
-  }
 
-  ngAfterViewInit():void {
-    const searchInput = document.getElementById("userSearch");
-    fromEvent(searchInput, "keyup").pipe(debounceTime(1000)).subscribe(value => {
-      this.searchUsers();
-    });
-  }
-
-  loadEmptyFormRequest():void {
-    this.formRequest = new FormGroup({
-      os: new FormControl('', [Validators.required]),
-      pcid: new FormControl('', [Validators.required]),
-      marca: new FormControl('', [Validators.required]),
-      modelo: new FormControl('', [Validators.required]),
-      serie: new FormControl('', [Validators.required]),
-      usuario: new FormControl('', [Validators.required]),
-    });
-  }
-
-  loadFormRequest():void {
-    this.formRequest = new FormGroup({
-      os: new FormControl(this.localRequest.os, [Validators.required]),
-      pcid: new FormControl({value: this.localRequest.pcid, disabled: true}, [Validators.required]),
-      marca: new FormControl({value: this.localRequest.marca, disabled: true}, [Validators.required]),
-      modelo: new FormControl({value: this.localRequest.modelo, disabled: true}, [Validators.required]),
-      serie: new FormControl({value: this.localRequest.serie, disabled: true}, [Validators.required]),
-      usuario: new FormControl({value: this.localRequest.usuario, disabled: true}, [Validators.required]),
-    });
-  }
-
-  obtainLicenses():void {
-    this.digitalToolsService.obtainLicenses(this.localRequest.id).subscribe(responseApi=>{
-      if(responseApi.body) {
-        this.dataSource = responseApi.body;
-      }
-    });
-  }
-  searchUsers():void {
-    this.users = [];
-    if(this.formRequest.value.usuario != null) {
-      if(this.formRequest.value.usuario.length>2){
-        this.isSearching = true;
-        this.digitalToolsService.userManagementByUsername(this.formRequest.value.usuario).subscribe(responseApi=>{
-          this.isSearching = false;
-          if(responseApi.body) {
-            if(responseApi.body.length > 0) {
-              for(var i=0; i<responseApi.body.length; ++i) {
-                this.users.push(responseApi.body[i]);
-              }
-            } else {
-              this.showSnackBar('No se encontraron coincidencias');
+    ngOnInit(): void {
+        this.action = localStorage.getItem('action');
+        if (this.action === 'edit') {
+            let localRequestStr = localStorage.getItem('usuario');
+            if (localRequestStr !== null && localRequestStr !== "") {
+                this.localRequest = JSON.parse(localRequestStr);
             }
-          } else {
-            this.showSnackBar('No se encontraron coincidencias');
-          }
-        });
-      }
-    }
-  }
-
-  selectUser(user:any):void {
-    this.user = user;
-  }
-
-  onListElectronicTools():void{
-    this.router.navigate(['/digital-tools/electronic-tools']);
-  }
-
-  onRegisterRequest():void{
-    if(this.formRequest.value.os==''){
-      const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese un OS válido!'},disableClose:true});
-    }else{
-      if(this.formRequest.value.pcid==''){
-        const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese un PCID válido!'},disableClose:true});
-      }else{
-        if(this.formRequest.value.marca==''){
-          const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese una Marca válida!'},disableClose:true});
-        }else{
-          if(this.formRequest.value.modelo==''){
-            const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese un Modelo válido!'},disableClose:true});
-          }else{    
-            if(this.formRequest.value.serie==''){
-              const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese una Serie válida!'},disableClose:true});
-            }else{      
-              if(this.formRequest.value.usuario==''){
-                const dialogError = this.matDialog.open(DialogErrorMessageComponent,{data:{text:'¡Ingrese un Usuario válido!'},disableClose:true});
-              }else{
-                const request = {
-                  dni: this.user.dni,
-                  bu: this.user.bu,
-                  correoJefe: this.user.correoJefe,
-                  jefe: this.user.jefe,
-                  ceco: this.user.centroCosto,
-                  licencias: this.dataSource,
-                  ...this.formRequest.value
-                };
-                if(this.action === 'edit') {
-                  request.id = this.localRequest.id;
-                  request.estado = this.localRequest.estado;
-                }
-                if(!this.user.dni) {
-                  request.dni = this.localRequest.dni;
-                } else {
-                  request.dni = this.user.dni;
-                }
-                this.digitalToolsService.toolManagement(request).subscribe(responseApi=>{
-                  const dialogRegistrarDatosDelUsuario = this.matDialog.open(DialogMassiveRegistrationSuccessfullyComponent,{
-                    data:{text:'Se envió el registro con éxito'},
-                    disableClose:true, width: '385px',
-                  })
-                  dialogRegistrarDatosDelUsuario.afterClosed().subscribe(responseDialog=>{
-                    if(responseDialog){
-                      this.router.navigate(['/digital-tools/electronic-tools']);
-                    }
-                  });
-                });
-              }
-            }
-          }
+            this.loadFormRequest();
+            this.obtainLicenses();
+            this.user = {
+                bu: this.localRequest.area,
+                centroCosto: this.localRequest.ceco,
+                jefe: this.localRequest.jefe,
+                correoJefe: this.localRequest.correoJefe
+            };
+        } else {
+            this.loadEmptyFormRequest();
         }
-      }
     }
-    
-  }
 
-  showSnackBar(message:string):void{
-    this.matSnackBar.openFromComponent(SnackBarMessageComponent, {
-      data: message,
-      duration: 3000,
-      horizontalPosition:'center',
-      verticalPosition: 'top',
-      panelClass:['mat-toolbar', 'mat-primary','button-color']
-    });
-  }
+    ngAfterViewInit(): void {
+        const searchInput = document.getElementById("userSearch");
+        fromEvent(searchInput, "keyup").pipe(debounceTime(1000)).subscribe(value => {
+            this.searchUsers();
+        });
+    }
+
+    loadEmptyFormRequest(): void {
+        this.formRequest = new FormGroup({
+            os: new FormControl('', [Validators.required]),
+            pcid: new FormControl('', [Validators.required]),
+            marca: new FormControl('', [Validators.required]),
+            modelo: new FormControl('', [Validators.required]),
+            serie: new FormControl('', [Validators.required]),
+            usuario: new FormControl('', [Validators.required]),
+        });
+    }
+
+    loadFormRequest(): void {
+        this.formRequest = new FormGroup({
+            os: new FormControl(this.localRequest.os, [Validators.required]),
+            pcid: new FormControl({value: this.localRequest.pcid, disabled: true}, [Validators.required]),
+            marca: new FormControl({value: this.localRequest.marca, disabled: true}, [Validators.required]),
+            modelo: new FormControl({value: this.localRequest.modelo, disabled: true}, [Validators.required]),
+            serie: new FormControl({value: this.localRequest.serie, disabled: true}, [Validators.required]),
+            usuario: new FormControl({value: this.localRequest.usuario, disabled: true}, [Validators.required]),
+        });
+    }
+
+    obtainLicenses(): void {
+        this.digitalToolsService.obtainLicenses(this.localRequest.id).subscribe(responseApi => {
+            if (responseApi.body) {
+                this.dataSource = responseApi.body;
+            }
+        });
+    }
+
+    searchUsers(): void {
+        this.users = [];
+        if (this.formRequest.value.usuario != null) {
+            if (this.formRequest.value.usuario.length > 2) {
+                this.isSearching = true;
+                this.digitalToolsService.userManagementByUsername(this.formRequest.value.usuario).subscribe(responseApi => {
+                    this.isSearching = false;
+                    if (responseApi.body) {
+                        if (responseApi.body.length > 0) {
+                            for (var i = 0; i < responseApi.body.length; ++i) {
+                                this.users.push(responseApi.body[i]);
+                            }
+                        } else {
+                            this.showSnackBar('No se encontraron coincidencias');
+                        }
+                    } else {
+                        this.showSnackBar('No se encontraron coincidencias');
+                    }
+                });
+            }
+        }
+    }
+
+    selectUser(user: any): void {
+        this.user = user;
+    }
+
+    onListElectronicTools(): void {
+        this.router.navigate(['/digital-tools/electronic-tools']);
+    }
+
+    onRegisterRequest(): void {
+        if (this.formRequest.value.os == '') {
+            const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                data: {text: '¡Ingrese un OS válido!'},
+                disableClose: true
+            });
+        } else {
+            if (this.formRequest.value.pcid == '') {
+                const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                    data: {text: '¡Ingrese un PCID válido!'},
+                    disableClose: true
+                });
+            } else {
+                if (this.formRequest.value.marca == '') {
+                    const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                        data: {text: '¡Ingrese una Marca válida!'},
+                        disableClose: true
+                    });
+                } else {
+                    if (this.formRequest.value.modelo == '') {
+                        const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                            data: {text: '¡Ingrese un Modelo válido!'},
+                            disableClose: true
+                        });
+                    } else {
+                        if (this.formRequest.value.serie == '') {
+                            const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                                data: {text: '¡Ingrese una Serie válida!'},
+                                disableClose: true
+                            });
+                        } else {
+                            if (this.formRequest.value.usuario == '') {
+                                const dialogError = this.matDialog.open(DialogErrorMessageComponent, {
+                                    data: {text: '¡Ingrese un Usuario válido!'},
+                                    disableClose: true
+                                });
+                            } else {
+                                const request = {
+                                    dni: this.user.dni,
+                                    bu: this.user.bu,
+                                    correoJefe: this.user.correoJefe,
+                                    jefe: this.user.jefe,
+                                    ceco: this.user.centroCosto,
+                                    licencias: this.dataSource,
+                                    ...this.formRequest.value
+                                };
+                                if (this.action === 'edit') {
+                                    request.id = this.localRequest.id;
+                                    request.estado = this.localRequest.estado;
+                                }
+                                if (!this.user.dni) {
+                                    request.dni = this.localRequest.dni;
+                                } else {
+                                    request.dni = this.user.dni;
+                                }
+                                this.digitalToolsService.toolManagement(request).subscribe(responseApi => {
+                                        const dialogRegistrarDatosDelUsuario = this.matDialog.open(DialogMassiveRegistrationSuccessfullyComponent, {
+                                            data: {text: 'Se envió el registro con éxito'},
+                                            disableClose: true, width: '385px',
+                                        })
+                                        dialogRegistrarDatosDelUsuario.afterClosed().subscribe(responseDialog => {
+                                            if (responseDialog) {
+                                                this.router.navigate(['/digital-tools/electronic-tools']);
+                                            }
+                                        });
+                                    },
+                                    error => {
+                                        console.log(error)
+                                    })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    showSnackBar(message: string): void {
+        this.matSnackBar.openFromComponent(SnackBarMessageComponent, {
+            data: message,
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            panelClass: ['mat-toolbar', 'mat-primary', 'button-color']
+        });
+    }
 }
--- src/app/shared/services/digital-tools/digital-tools.service.ts
+++ src/app/shared/services/digital-tools/digital-tools.service.ts
@@ -1,61 +1,69 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { AuthService } from 'app/core/auth/auth.service';
-import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {AuthService} from 'app/core/auth/auth.service';
+import {environment} from 'environments/environment';
+import {Observable} from 'rxjs';
 import internal from 'stream';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DigitalToolsService {
 
-  header = new HttpHeaders({
-    'Authorization': this.authService.accessToken
-  });
+    header = new HttpHeaders({
+        'Authorization': this.authService.accessToken
+    });
 
-  dateCurrent = new Date();
+    dateCurrent = new Date();
 
-  private baseApiURl = environment.apiUrl;
-  private urlToolManagement = this.baseApiURl + '/Mantenimiento/MantenimientoHerramientas';
-  private urlToolUserManagement = this.baseApiURl + '/Mantenimiento/MantenimientoHerramientaUsuario';
-  private urlUserListManagement = this.baseApiURl + '/Mantenimiento/ObtenerListadoUsuarios';
-  private urlUserManagement = this.baseApiURl + '/Mantenimiento/ObtenerUsuario';
-  private urlTrayTools = this.baseApiURl + '/Mantenimiento/BandejaHerramientas';
-  private urlObtainLicenses = this.baseApiURl + '/Mantenimiento/ObtenerLicencias';
+    private baseApiURl = environment.apiUrl;
+    private urlToolManagement = this.baseApiURl + '/Mantenimiento/MantenimientoHerramientas';
+    private urlToolUserManagement = this.baseApiURl + '/Mantenimiento/MantenimientoHerramientaUsuario';
+    private urlUserListManagement = this.baseApiURl + '/Mantenimiento/ObtenerListadoUsuarios';
+    private urlUserManagement = this.baseApiURl + '/Mantenimiento/ObtenerUsuario';
+    private urlTrayTools = this.baseApiURl + '/Mantenimiento/BandejaHerramientas';
+    private urlObtainLicenses = this.baseApiURl + '/Mantenimiento/ObtenerLicencias';
 
-  constructor(private readonly httpClient: HttpClient,
-    private readonly authService: AuthService) { }
+    constructor(private readonly httpClient: HttpClient,
+                private readonly authService: AuthService) {
+    }
 
-  toolManagement(_request: any): Observable<any> {
-    debugger
-    return this.httpClient.post(this.urlToolManagement, _request, { headers: this.header });
-  }
-  toolUserManagement(_request: any): Observable<any> {
-    return this.httpClient.post(this.urlToolUserManagement, _request, { headers: this.header });
-  }
-  userListManagement(filter, _page): Observable<any> {
-    const request = { filter: filter, page: (_page - 1), pageSize: 10 };
-    return this.httpClient.post(this.urlUserListManagement, request, { headers: this.header });
-  }
-  userManagementByDocument(dni:string): Observable<any> {
-    return this.userManagement(1, dni);
-  }
-  userManagementByUsername(usr:string): Observable<any> {
-    return this.userManagement(2, usr);
-  }
-  userManagement(code:number, search:string): Observable<any> {
-    return this.httpClient.get(this.urlUserManagement+'/'+code+'/'+search,{headers:this.header});
-  }
-  trayTools(filter, _page): Observable<any> {
-    const request = { filter: filter, page: (_page - 1), pageSize: 10 };
-    return this.httpClient.post(this.urlTrayTools, request, { headers: this.header });
-  }
-  searchUserByUsername(usr:string): Observable<any> {
-    const request = { filter: { usuario: usr }, page: 0, pageSize: 10 };
-    return this.httpClient.post(this.urlUserListManagement, request, { headers: this.header });
-  }
-  obtainLicenses(toolId:string): Observable<any> {
-    return this.httpClient.get(this.urlObtainLicenses+'/'+toolId, { headers: this.header });
-  }
+    toolManagement(_request: any): Observable<any> {
+        return this.httpClient.post(this.urlToolManagement, _request, {headers: this.header})
+    }
+
+    toolUserManagement(_request: any): Observable<any> {
+        return this.httpClient.post(this.urlToolUserManagement, _request, {headers: this.header});
+    }
+
+    userListManagement(filter, _page): Observable<any> {
+        const request = {filter: filter, page: (_page - 1), pageSize: 10};
+        return this.httpClient.post(this.urlUserListManagement, request, {headers: this.header});
+    }
+
+    userManagementByDocument(dni: string): Observable<any> {
+        return this.userManagement(1, dni);
+    }
+
+    userManagementByUsername(usr: string): Observable<any> {
+        return this.userManagement(2, usr);
+    }
+
+    userManagement(code: number, search: string): Observable<any> {
+        return this.httpClient.get(this.urlUserManagement + '/' + code + '/' + search, {headers: this.header});
+    }
+
+    trayTools(filter, _page): Observable<any> {
+        const request = {filter: filter, page: (_page - 1), pageSize: 10};
+        return this.httpClient.post(this.urlTrayTools, request, {headers: this.header});
+    }
+
+    searchUserByUsername(usr: string): Observable<any> {
+        const request = {filter: {usuario: usr}, page: 0, pageSize: 10};
+        return this.httpClient.post(this.urlUserListManagement, request, {headers: this.header});
+    }
+
+    obtainLicenses(toolId: string): Observable<any> {
+        return this.httpClient.get(this.urlObtainLicenses + '/' + toolId, {headers: this.header});
+    }
 }
